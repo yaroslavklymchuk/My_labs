d2b5eb4c4b9146696b815043cb123250155fece2
Yaroslav Klymchuk <klm.98yar@gmail.com>
2017-03-11 20:56:10 +0200

    Second Commit

---
+++ SFML_WIN/Heroes.h
@@ -0,0 +1,86 @@
+//TODO 
+//Create function Get doodle_sprite!!!!
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <time.h>
+
+using namespace sf;
+
+
+class Doodle
+{
+public:
+	Sprite doodle_sprite;
+	Doodle(String, float, float, float, float, float, float);
+	~Doodle();
+
+private:
+	float x_l, y_l, x_r, y_r, positionX, positionY;
+	String file_name;
+	Image doodle_im;
+	Texture doodle_tex;
+	//Sprite doodle_sprite;
+
+};
+
+Doodle::Doodle(String file, float x_left, float y_left, float x_right, float y_right, float x_, float y_){
+	doodle_im.loadFromFile("f:/C++SFML/SFML_WIN/images/" + file);
+	doodle_tex.loadFromImage(doodle_im);
+	doodle_sprite.setTexture(doodle_tex);
+	x_l = x_left;
+	y_l = y_left;
+	x_r = x_right;
+	y_r = y_right;
+	positionX = x_;
+	positionY = y_;
+	doodle_sprite.setTextureRect(IntRect(x_l, y_l, x_r, y_r));
+	doodle_sprite.setPosition(positionX, positionY);
+}
+
+Doodle::~Doodle(){
+
+}
+
+class Enemies{
+
+public:
+	Sprite enemy_sprite;
+	Enemies(float, float, float, float, float, float);
+	~Enemies();
+
+private:
+	float x_l, y_l, x_r, y_r, positionX, positionY;
+	//String file_names [20];
+	Image enemy_im;
+	Texture enemy_tex;
+};
+
+Enemies::Enemies(float x_left, float y_left, float x_right, float y_right, float x_, float y_) {
+	String file[5];
+	file[0] = "enemy_1.png";
+	file[1] = "enemy_2.png";
+	file[2] = "enemy_3.png";
+	file[3] = "enemy_4.png";
+	file[4] = "enemy_5.png";
+	
+	srand(time(0));
+	int i = 0;
+	i = rand() % 5;
+	enemy_im.loadFromFile("f:/C++SFML/SFML_WIN/images/" + file[i]);
+	enemy_im.createMaskFromColor(Color::White);
+	enemy_tex.loadFromImage(enemy_im);
+	enemy_sprite.setTexture(enemy_tex);
+	x_l = x_left;
+	y_l = y_left;
+	x_r = x_right;
+	y_r = y_right;
+	positionX = x_;
+	positionY = y_;
+	enemy_sprite.setTextureRect(IntRect(x_l, y_l, x_r, y_r));
+	enemy_sprite.setPosition(positionX, positionY);
+}
+
+Enemies::~Enemies(){
+
+}
--- SFML_WIN/SFML_wincpp.cpp
+++ SFML_WIN/SFML_wincpp.cpp
@@ -1,5 +1,7 @@
+//TODO with classes for heroes
 #include <SFML/Graphics.hpp>
 #include <time.h>
+#include "Heroes.h"
 
 using namespace sf;
 int main()
@@ -12,72 +14,79 @@ int main()
 
 	struct plato platforms[100];
 
-	//задаем рандомные координаты платформам
+	//srand(time(0));
+
+	//Set coordinates to platforms
 	for (int i = 0; i < 10; i++) {
 		platforms[i].x = rand() % 500;
 		platforms[i].y = rand() % 500;
 	}
 
-	srand(time(0));
-
 	RenderWindow window(VideoMode(500, 500), "SFMLworks", Style::Fullscreen);
-	
-	Image doodle, background, platform, doodle_fire;
-	doodle.loadFromFile("f:/C++SFML/SFML_WIN/images/Doodler.png");
+	//window.setFramerateLimit(1500);
+
+	Image background, platform, doodle_fire;
 	background.loadFromFile("f:/C++SFML/SFML_WIN/images/background_fone.jpg");
 	platform.loadFromFile("f:/C++SFML/SFML_WIN/images/Doodle_Platform_green.png");
 	platform.createMaskFromColor(Color::White);
-	//doodle_im1.createMaskFromColor(Color(0, 0, 0));
 
-	Texture doodle_tex, background_tex, platform_tex;
-	doodle_tex.loadFromImage(doodle);
+	Texture background_tex, platform_tex;
 	background_tex.loadFromImage(background);
 	platform_tex.loadFromImage(platform);
 
-	Sprite doodle_sprite, background_sprite, platform_sprite;
-	doodle_sprite.setTexture(doodle_tex);
+	Sprite background_sprite, platform_sprite;
 	background_sprite.setTexture(background_tex);
 	platform_sprite.setTexture(platform_tex);
-	doodle_sprite.setTextureRect(IntRect(15, 15, 193, 180));
 	background_sprite.setTextureRect(IntRect(0, 0, 300, 300));
-	doodle_sprite.setPosition(500,500);
-	
+
+	Doodle doodle1("Doodler.png", 15, 15, 193, 180, 100,100);
+	for (int i = 0; i < 10; i++) {
+		platform_sprite.setPosition(platforms[i].x, platforms[i].y);
+	}
+
+	Enemies enemy = Enemies(10, 10, 200, 190, platforms[3].x, platforms[3].y + 150);
 	
 
-
+	
+	//TODO problem with SFML_time
+	Clock clock;
+//	float time = clock.getElapsedTime().asMicroseconds();
 	while (window.isOpen())
 	{
 		Event event;
 		while (window.pollEvent(event))
 		{
+			float time = clock.getElapsedTime().asMicroseconds();
+			clock.restart();
+			time = time / 800;
 			if (event.type == Event::Closed || Keyboard::isKeyPressed(Keyboard::Escape))
 				window.close();
 		}
 
 		if ((Keyboard::isKeyPressed(Keyboard::Right)) || (Keyboard::isKeyPressed(Keyboard::D))) {
-			doodle_sprite.setTextureRect(IntRect(15, 15, 193, 180));
-			doodle_sprite.move(0.1, 0);
+			doodle1.doodle_sprite.setTextureRect(IntRect(15, 15, 193, 180));
+			doodle1.doodle_sprite.move(0.1, 0);
 		}
 		if ((Keyboard::isKeyPressed(Keyboard::Left)) || (Keyboard::isKeyPressed(Keyboard::A))) {
-			doodle_sprite.setTextureRect(IntRect(208, 15, -193, 180));
-			doodle_sprite.move(-0.1, 0);
+			doodle1.doodle_sprite.setTextureRect(IntRect(208, 15, -193, 180));
+			doodle1.doodle_sprite.move(-0.1, 0);
 		}
 		if ((Keyboard::isKeyPressed(Keyboard::Up)) || (Keyboard::isKeyPressed(Keyboard::W))) {
-			doodle_sprite.move(0, -0.1);
+			doodle1.doodle_sprite.move(0, -0.1);
 		}
 		if ((Keyboard::isKeyPressed(Keyboard::Down)) || (Keyboard::isKeyPressed(Keyboard::S))) {
-			doodle_sprite.move(0, 0.1);
+			doodle1.doodle_sprite.move(0, 0.1);
 		}
 
 		window.clear();
-		window.draw(doodle_sprite);
-		window.draw(background_sprite);
+		window.draw(doodle1.doodle_sprite);
+	//	window.draw(background_sprite);
 		for (int i = 0; i < 10; i++) {
 			platform_sprite.setPosition(platforms[i].x, platforms[i].y);
 			window.draw(platform_sprite);
 		}
-
-		//window.draw(platform_sprite);
+		window.draw(platform_sprite);
+		window.draw(enemy.enemy_sprite);
 		window.display();
 	}
 
